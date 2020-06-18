63b1f5ce22db2bd0903a0d2a6f05e446ea976d56
rafaelitajahy <rafaelitajahy@gmail.com>
2020-06-18 20:09:49 -0300

    Recuperação de senha

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -72,7 +72,7 @@ describe('AuthenticateUser', () => {
       password: '123456',
     });
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@gmail.com',
         password: 'wrong-password',
