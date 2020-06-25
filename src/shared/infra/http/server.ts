7c76ce1971bdd726e9a4a3a506542b0c592df4bb
rafaelitajahy <rafaelitajahy@gmail.com>
2020-06-25 19:15:56 -0300

    update

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,15 @@
 import 'reflect-metadata';
-// import 'dotenv/config';
+import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
-// import { errors } from 'celebrate';
+import { errors } from 'celebrate';
 import cors from 'cors';
 
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
-// import rateLimiter from './middlewares/rateLimiter';
+import rateLimiter from './middlewares/rateLimiter';
 
 import routes from './routes';
 
@@ -21,10 +21,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
-// app.use(rateLimiter);
+app.use(rateLimiter);
 app.use(routes);
 
-// app.use(errors());
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
